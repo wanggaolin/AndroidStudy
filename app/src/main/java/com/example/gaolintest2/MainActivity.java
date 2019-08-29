b0919273af76d63938298ce2e4425da272c25ac3
gaolin <gaolin@6.cn>
2019-08-29 15:14:35 +0800

    新增学习结果

--- app/src/main/java/com/example/gaolintest2/MainActivity.java
+++ app/src/main/java/com/example/gaolintest2/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button Buttoncalculate;
+    private Button ButtonQQLogin;
+    private Button ButtonQQLogin1;
+    private Button ButtonFormInput;
+    private Button ButtonBlossom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,Calculate.class);
+                startActivity(intent);
+            }
+        });
+
+        ButtonQQLogin = (Button) findViewById(R.id.qq_login);
+        ButtonQQLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,QQLoginPage.class);
+                startActivity(intent);
+            }
+        });
+
+        ButtonQQLogin1 = (Button) findViewById(R.id.qq_login1);
+        ButtonQQLogin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,QQLoginPage1.class);
+                startActivity(intent);
+            }
+        });
+
+        ButtonFormInput = (Button) findViewById(R.id.form_input);
+        ButtonFormInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,FromInput.class);
+                startActivity(intent);
+            }
+        });
+
+        ButtonBlossom = (Button) findViewById(R.id.blossom_image);
+        ButtonBlossom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,BlossomImage.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/gaolintest2/QQLoginPage1.java
@@ -0,0 +1,46 @@
+package com.example.gaolintest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class QQLoginPage1 extends AppCompatActivity {
+
+    private Button butSubmit;
+    private EditText qq_number,qq_pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qqlogin_page1);
+        initview();
+        butSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String qq_tel = qq_number.getText().toString();
+                String qq_password = qq_pwd.getText().toString();
+                Log.i("QQLoginPage1","登录输入的账号和密码");
+                Log.i("QQLoginPage1",qq_tel);
+                Log.i("QQLoginPage1",qq_password);
+                if(qq_tel.equals("110") && qq_password.equals("110")){
+                    Toast toast = Toast.makeText(getApplicationContext(),R.string.qq_login_success,Toast.LENGTH_SHORT);
+                    toast.show();
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(),R.string.qq_login_fail,Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+    }
+
+    void initview(){
+        butSubmit = (Button) findViewById(R.id.submit);
+        qq_number = (EditText) findViewById(R.id.qq_number);
+        qq_pwd = (EditText) findViewById(R.id.qq_pwd);
+    }
+}
